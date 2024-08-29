c73d7bd0a1b68b53ba98c919788852799f142bc5
Jose Laurito <jigolaurito93@yahoo.com>
2024-08-29 00:13:41 -0500

    submitButton component

--- app/profile/create/page.tsx
+++ app/profile/create/page.tsx
@@ -1,3 +1,5 @@
+import SubmitButton from "@/components/form/Buttons";
+import FormInput from "@/components/form/FormInput";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
@@ -15,10 +17,8 @@ const CreateProfilePage = () => {
       <h1 className="text-2xl font-semibold mb-8 capitalize">new user</h1>
       <div className="border p-8 rounded-md max-w-lg">
         <form action={createProfileAction}>
- 
-          <Button type="submit" size="lg">
-            Create Profile
-          </Button>
+          <FormInput />
+          <SubmitButton />
         </form>
       </div>
     </section>
